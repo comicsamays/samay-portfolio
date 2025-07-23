92301081b201f3ee689becfd7da32d7df51dbb92
comicsamays <samay_magecha@outlook.com>
2025-07-23 15:02:55 +0100

    feat: about me data file added

--- src/about/AboutMenu.js
+++ src/about/AboutMenu.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import AboutMenuItem from "./AboutMenuItem"
+import AboutSubheading from "./AboutSubheading"
+import SubheadingsData from "./SubheadingsData"
 
 export default class AboutMenu extends Component {
   render() {
---
+++ src/about/SubheadingsData.js
@@ -0,0 +1,77 @@
+const subheadingsData = {
+    1: [
+        {
+            title: "About Me",
+            content: (
+                <>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices eu turpis vel vulputate. Vestibulum fringilla enim id ultricies porta. Quisque congue ante eu nisl finibus bibendum. Phasellus eget magna urna. Nam molestie viverra lobortis. Maecenas a pretium diam, ac congue lacus. Duis molestie neque neque, vitae mollis diam congue sit amet. 
+                    </p>
+                </>
+            ),
+        },
+    ],
+
+    2: [
+        {
+            title: "2025 Bachelors Degree",
+            content: (
+                <>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices eu turpis vel vulputate. Vestibulum fringilla enim id ultricies porta. Quisque congue ante eu nisl finibus bibendum. Phasellus eget magna urna. Nam molestie viverra lobortis. Maecenas a pretium diam, ac congue lacus. Duis molestie neque neque, vitae mollis diam congue sit amet. 
+                    </p>
+                </>
+            ),
+        },
+
+        {
+            title: "2021 A-Level",
+            content: (
+                <>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices eu turpis vel vulputate. Vestibulum fringilla enim id ultricies porta. Quisque congue ante eu nisl finibus bibendum. Phasellus eget magna urna. Nam molestie viverra lobortis. Maecenas a pretium diam, ac congue lacus. Duis molestie neque neque, vitae mollis diam congue sit amet. 
+                    </p>
+                </>
+            ),
+        },
+
+        {
+            title: "2019 GCSEs",
+            content: (
+                <>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices eu turpis vel vulputate. Vestibulum fringilla enim id ultricies porta. Quisque congue ante eu nisl finibus bibendum. Phasellus eget magna urna. Nam molestie viverra lobortis. Maecenas a pretium diam, ac congue lacus. Duis molestie neque neque, vitae mollis diam congue sit amet. 
+                    </p>
+                </>
+            ),
+        },
+    ],
+
+    3: [
+        {
+            title: "2022 Software Engineering Team Lead",
+            content: (
+                <>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices eu turpis vel vulputate. Vestibulum fringilla enim id ultricies porta. Quisque congue ante eu nisl finibus bibendum. Phasellus eget magna urna. Nam molestie viverra lobortis. Maecenas a pretium diam, ac congue lacus. Duis molestie neque neque, vitae mollis diam congue sit amet. 
+                    </p>
+                </>
+            ),
+        },
+
+        {
+            title: "2023 Site Reliability Engineer",
+            content: (
+                <>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices eu turpis vel vulputate. Vestibulum fringilla enim id ultricies porta. Quisque congue ante eu nisl finibus bibendum. Phasellus eget magna urna. Nam molestie viverra lobortis. Maecenas a pretium diam, ac congue lacus. Duis molestie neque neque, vitae mollis diam congue sit amet. 
+                    </p>
+                </>
+            ),
+        },
+
+    ],
+    
+};
+
+export default subheadingsData
