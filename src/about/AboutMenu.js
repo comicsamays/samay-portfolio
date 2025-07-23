6988f520c115b9e66815c5542a0a03328a77b701
comicsamays <samay_magecha@outlook.com>
2025-07-23 15:56:56 +0100

    bugfix: not rendering about section

--- src/about/AboutMenu.js
+++ src/about/AboutMenu.js
@@ -1,12 +1,75 @@
 import React, { Component } from 'react'
 import AboutMenuItem from "./AboutMenuItem"
 import AboutSubheading from "./AboutSubheading"
-import SubheadingsData from "./SubheadingsData"
+import personalIcon from "../assets/moebius-triangle.png"
+import educationIcon from "../assets/upgrade.png"
+import careerIcon from "../assets/triple-corn.png"
+import subheadingsData from './SubheadingsData'
 
 export default class AboutMenu extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    activeMenuItem: 1,
+    activeSubheading: 1,
+    };
+  }
+
+  handleMenuItemClick = (menuItem) => {
+    this.setState({
+      activeMenuItem: menuItem,
+      activeSubheading: 1,
+    });
+  };
+
+  handleSubHeadingClick = (subheading) => {
+    this.setState({
+      activeSubheading: subheading,
+    });
+  };
+
   render() {
+    const {activeMenuItem, activeSubheading} = this.state;
+    const menuItems = ["PERSONAL", "EDUCATION", "CAREER"];
+    const activeMenuTitle = menuItems[activeMenuItem - 1];
+    const activeMenuIcon =
+      activeMenuTitle === "PERSONAL" ? personalIcon : 
+      activeMenuTitle === "EDUCATION" ? educationIcon :
+      careerIcon;
+
+      const subheadings = subheadingsData[activeMenuItem];
+
     return (
-      <div>AboutMenu</div>
+      <>
+        <div className="menu">
+          {menuItems.map((item, index) => (
+            <AboutMenuItem
+              key={index}
+              title={item}
+              active={activeMenuItem === index + 1}
+              onClick={() => this.handleMenuItemClick(index + 1)}
+            />
+          ))}
+        </div>
+        <div className="sub-container">
+          <div className="icon-title-container">
+            <img src={activeMenuIcon} alt={activeMenuTitle}
+            className="icon"/>
+            <h3>{activeMenuTitle}</h3>
+          </div>
+          {subheadings.map((subheading, index) => (
+            <AboutSubheading
+              key={index}
+              title={subheading.title}
+              content={subheading.content}
+              active={activeSubheading === index + 1}
+              onClick={() => this.handleSubHeadingClick(index + 1)}
+              menuItem = {activeMenuItem}
+            />
+          ))}
+        </div>
+      </>
     )
   }
 }
