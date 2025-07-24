a91d87e6c8cd09ea604e0949a6c24cf46c916070
comicsamays <samay_magecha@outlook.com>
2025-07-24 21:31:26 +0100

    style: formatted About Page

--- src/about/SubheadingsData.js
+++ src/about/SubheadingsData.js
@@ -5,7 +5,21 @@ const subheadingsData = {
             content: (
                 <>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices eu turpis vel vulputate. Vestibulum fringilla enim id ultricies porta. Quisque congue ante eu nisl finibus bibendum. Phasellus eget magna urna. Nam molestie viverra lobortis. Maecenas a pretium diam, ac congue lacus. Duis molestie neque neque, vitae mollis diam congue sit amet. 
+                        Hey there! I'm Samay, a computer science graduate with a mind too fast for my mouth. My interests lie in ideation and creatively solving problems with real world impact. I'm a huge people person; teams and working with people are my preference, though I am very capable of seeing work through from start to finish individually.
+                    <br></br> <br></br>
+                    Interests:
+                    <ul>
+                        <li>Reading </li>
+                        <li>Gym </li>
+                        <li>Music </li>
+                        <li>Water Sports </li>
+                        <li>Volleyball </li>
+                        <li>Combat Sports </li>
+                        <li>Dogs </li>
+                        <li>Running </li>
+                        <li>DOOM </li>
+                        <li>Storytelling</li>
+                    </ul>
                     </p>
                 </>
             ),
@@ -16,10 +30,17 @@ const subheadingsData = {
         {
             title: "2025 Bachelors Degree",
             content: (
-                <>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices eu turpis vel vulputate. Vestibulum fringilla enim id ultricies porta. Quisque congue ante eu nisl finibus bibendum. Phasellus eget magna urna. Nam molestie viverra lobortis. Maecenas a pretium diam, ac congue lacus. Duis molestie neque neque, vitae mollis diam congue sit amet. 
-                    </p>
+                <> 
+                <p>
+                    Computer Science with a Year in Industry BSc <br></br>
+                    First Class Honours <br></br>
+                    2021-2025
+                     <ul>
+                        <li>Software Engineering Group Project with IBM</li>
+                        <li>Placement Year with ServiceNow as a Site Reliability Engineer</li>
+                        <li>Dissertation on running Human-LLM interaction experiments</li>
+                    </ul>
+                </p>
                 </>
             ),
         },
@@ -28,9 +49,14 @@ const subheadingsData = {
             title: "2021 A-Level",
             content: (
                 <>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices eu turpis vel vulputate. Vestibulum fringilla enim id ultricies porta. Quisque congue ante eu nisl finibus bibendum. Phasellus eget magna urna. Nam molestie viverra lobortis. Maecenas a pretium diam, ac congue lacus. Duis molestie neque neque, vitae mollis diam congue sit amet. 
-                    </p>
+                <p>
+                    Mill Hill School Foundation
+                <ul>
+                    <li>Mathematics - A*</li>
+                    <li>Computer Science - A*</li>
+                    <li>Physics - A</li>
+                </ul>
+                </p>
                 </>
             ),
         },
@@ -39,9 +65,10 @@ const subheadingsData = {
             title: "2019 GCSEs",
             content: (
                 <>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices eu turpis vel vulputate. Vestibulum fringilla enim id ultricies porta. Quisque congue ante eu nisl finibus bibendum. Phasellus eget magna urna. Nam molestie viverra lobortis. Maecenas a pretium diam, ac congue lacus. Duis molestie neque neque, vitae mollis diam congue sit amet. 
-                    </p>
+                      Mill Hill School Foundation
+                <ul>
+                    <li>10 GCSE's at: A* - B</li>
+                </ul>
                 </>
             ),
         },
@@ -49,23 +76,34 @@ const subheadingsData = {
 
     3: [
         {
-            title: "2022 Software Engineering Team Lead",
+            title: "2023 Site Reliability Engineer",
             content: (
-                <>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices eu turpis vel vulputate. Vestibulum fringilla enim id ultricies porta. Quisque congue ante eu nisl finibus bibendum. Phasellus eget magna urna. Nam molestie viverra lobortis. Maecenas a pretium diam, ac congue lacus. Duis molestie neque neque, vitae mollis diam congue sit amet. 
-                    </p>
+                <> <p>
+                    ServiceNow <br></br>
+                    June 2023 - June 2024
+                <ul>
+                    <li>Developed a real-time SRE Metrics dashboard in JavaScript</li>
+                    <li> Independently resolved 50+ time-sensitive tickets and alerts monthly.</li>
+                    <li>Managed weekly resourcing for a 20-member SRE EMEA team, ensuring on-time task and alert handling. </li>
+                    <li> Independently and rapidly upskilled in UNIX/Linux terminal usage and MYSQL database management</li>
+                </ul>
+                </p>
                 </>
             ),
         },
 
         {
-            title: "2023 Site Reliability Engineer",
+            title: "2022 Software Engineering Team Lead",
             content: (
                 <>
+                <p>
+                AR Platform for Teaching Quantum Computing <br></br>
+                IBM <br></br>
+                October 2022 - May 2023
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices eu turpis vel vulputate. Vestibulum fringilla enim id ultricies porta. Quisque congue ante eu nisl finibus bibendum. Phasellus eget magna urna. Nam molestie viverra lobortis. Maecenas a pretium diam, ac congue lacus. Duis molestie neque neque, vitae mollis diam congue sit amet. 
+                        I was the team lead for this project, for which we used Node.js, AR.js & THREE.js. I ensured agile project delivery through SCRUM and KANBAN practices, achieving and exceeding milestones for all sprints. I leveraged IBM's cloud APIs such as Watson TTS & AI to communicate with users.
                     </p>
+                </p>
                 </>
             ),
         },
