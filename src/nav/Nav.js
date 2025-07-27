ee4215df16da1e54b399d836e24c758a08bb2d56
comicsamays <samay_magecha@outlook.com>
2025-07-27 21:14:11 +0100

    feat: contact page

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,4 +1,3 @@
-import React, { Component } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import astronautHelmet from "../assets/astronaut-helmet.png"
 import deadEye from "../assets/dead-eye.png"
