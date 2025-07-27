b91eb1c841ecdf87ba52a8479d424b3063735a93
comicsamays <samay_magecha@outlook.com>
2025-07-27 17:10:14 +0100

    feat: initial projects data added

---
+++ src/projects/ProjectsData.js
@@ -0,0 +1,47 @@
+import projectOne from "../assets/project-1.png";
+import projectTwo from "../assets/project-2.png";
+import projectThree from "../assets/project-3.png";
+
+const projects = {
+    1: {
+        title: "Dissertation",
+        image: projectOne,
+        description: (
+            <>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam viverra feugiat est, eu consectetur velit lacinia quis. Mauris bibendum eu odio sit amet condimentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean massa libero, posuere porttitor aliquam ac, eleifend malesuada turpis.
+                </p>
+            </>
+        ),
+        github: "https://github.com",
+        demo: "https://youtube.com",
+    },
+
+    2: {
+        title: "Augmented Reality Platform to teach Quantum Computing",
+        image: projectTwo,
+        description: (
+            <>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam viverra feugiat est, eu consectetur velit lacinia quis. Mauris bibendum eu odio sit amet condimentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean massa libero, posuere porttitor aliquam ac, eleifend malesuada turpis.
+                </p>
+            </>
+        ),
+        github: "https://github.com",
+        demo: "https://youtube.com",
+    },
+
+    3: {
+        title: "SRE Metrics Dashboard",
+        image: projectThree,
+        description: (
+            <>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam viverra feugiat est, eu consectetur velit lacinia quis. Mauris bibendum eu odio sit amet condimentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean massa libero, posuere porttitor aliquam ac, eleifend malesuada turpis.
+                </p>
+            </>
+        ),
+        github: "https://github.com",
+        demo: "https://youtube.com",
+    },
+};
