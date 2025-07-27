1b14967224715d39c5b37698b78e18a3c234c55e
comicsamays <samay_magecha@outlook.com>
2025-07-27 11:45:15 +0100

    feat: Skills menu set up

---
+++ src/skills/SkillsData.js
@@ -0,0 +1,61 @@
+const skills = {
+    1: [ 
+        {
+            title: "HTML, CSS, JS",
+            level: 4,
+        },
+
+        {
+            title: "Procedural",
+            level: 5,
+        },
+    
+        {
+            title: "OOP",
+            level: 4,
+        },
+    
+        {
+            title: "Functional",
+            level: 3,
+        },
+
+        {
+            title: "Leetcode",
+            level: 3,
+        },
+    ],
+
+    2: [ 
+        {
+            title: "MySQL",
+            level: 5,
+        },
+        {
+            title: "Git",
+            level: 6,
+        },
+
+        {
+            title: "ServiceNow",
+            level: 5,
+        },
+    
+        {
+            title: "Azure",
+            level: 3,
+        },
+
+        {
+            title: "Figma",
+            level: 5,
+        },
+
+         {
+            title: "Trello",
+            level: 4,
+        },
+    ],
+};
+
+export default skills
